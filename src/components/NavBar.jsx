f966b85a6edbae3e11efeb38391a5c3edd5de912
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-04 14:00:26 +0300

    changed the navbar links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,21 @@
+
 import React from "react";
+import { Link } from "react-router-dom"; 
 
 const NavBar = () => {
   return (
     <>
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="./Products">Products</a></li>
-        <li><a href="/Wishlist">Wishlist</a></li>
-      </ul>
-    </nav>
- 
+      <nav>
+        <ul>
+
+          <li><Link className="btn1" to="/">Home</Link></li>
+          <li><Link className="btn2" to="/products">Products</Link></li>
+          <li><Link className="btn3" to="/wishlist">Wishlist</Link></li>
+        </ul>
+      </nav>
     </>
   );
 };
 
 export default NavBar;
-
-
 
