6cea7099d9c420358def1f3fc3df5fab21cc6fdd
Alex-gikungu <alexigikungu.012@gmail.com>
2023-08-04 13:46:29 +0300

    changed the cover image to background

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,31 +3,34 @@ import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 
+
 function HomePage() {
   return (
     <>
       <NavBar />
-      <div className="cover-page">
-        <div className="logo">
-          <img src="1.png" />
-        </div>
+      <div className="background-image"> {/* Add a class name for the container */}
+        <div className="cover-page">
+          <div className="logo">
+            <img src="1.png" alt="Logo" />
+          </div>
 
-        <div className="logo_s">
-          <button className="btn_0">KICKOFF MART</button>
-          <p>-Your one stop shop for every sportswear-</p>
-        </div>
+          <div className="logo_s">
+            <button className="btn_0">KICKOFF MART</button>
+            <p>-Your one-stop shop for every sportswear-</p>
+          </div>
 
-        <div className="btn_1">
-          {/* Use Link components instead of buttons */}
-          <Link className="btn1" to="/">
-            HOME
-          </Link>
-          <Link className="btn2" to="/products">
-            PRODUCTS
-          </Link>
-          <Link className="btn3" to="/wishlist">
-            WISHLIST
-          </Link>
+          <div className="btn_1">
+            {/* Use Link components instead of buttons */}
+            <Link className="btn1" to="/">
+              HOME
+            </Link>
+            <Link className="btn2" to="/products">
+              PRODUCTS
+            </Link>
+            <Link className="btn3" to="/wishlist">
+              WISHLIST
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -37,7 +40,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-
-
-
